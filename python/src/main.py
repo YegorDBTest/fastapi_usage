8e0cee86a047fd4abae70d7d2b829803afbfd432
Yegor Bitensky <yegorbitensky@gmail.com>
2022-01-16 23:58:40 +0300

    Add with body requests handlers

--- python/src/main.py
+++ python/src/main.py
@@ -2,11 +2,20 @@ from enum import Enum
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 app = FastAPI()
@@ -43,6 +52,22 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        item_dict.update({
+            "price_with_tax": item.price + item.tax
+        })
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
 @app.get("/item/{item_id}")
