3674b1a4be39329835bf9ac7ddb419a8366b93a4
Yegor Bitensky <yegorbitensky@gmail.com>
2022-02-03 23:57:28 +0300

    Add default exception handlers usage

--- python/src/main.py
+++ python/src/main.py
@@ -7,9 +7,17 @@ from fastapi import (
     Body, Cookie, FastAPI, Form, Header, Path, Query, status, File, UploadFile,
     HTTPException, Request,
 )
-from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 from pydantic import BaseModel, EmailStr, Field, HttpUrl
+
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 
 class UnicornException(Exception):
@@ -84,6 +92,30 @@ async def unicorn_exception_handler(request: Request, exc: UnicornException):
         content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
     )
 
+# @app.exception_handler(StarletteHTTPException)
+# async def http_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG! An HTTP error!: {repr(exc)}")
+    return await http_exception_handler(request, exc)
+
+
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request: Request, exc: RequestValidationError):
+#     return JSONResponse(
+#         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+#         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+#     )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
+
 
 @app.get("/")
 async def root():
@@ -147,7 +179,7 @@ async def read_item(
             detail="Item not found",
             headers={"X-Error": "There goes my error"},
         )
-    elif item_id = 20:
+    elif item_id == 20:
         raise UnicornException(item_id=item_id)
     item = {"item_id": item_id, "needy": needy}
     if q:
